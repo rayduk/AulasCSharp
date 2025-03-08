f5b767df5bfa42ce74467905f6a3b738d199b8a0
Rayduk <rayduferreira@gmail.com>
2025-03-07 22:22:17 -0300

    Conta Corrente

---
+++ Projeto Conta Corrente/Conta Corrente/Conta Corrente/ContaCorrente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Corrente
+{
+    internal class ContaCorrente
+    {
+        public int NumeroConta;
+        public string Titular;
+        public double Saldo;
+
+        public ContaCorrente(double saldo, int numero, string titular) {
+
+            Titular = titular;
+            NumeroConta = numero;
+            Saldo = saldo;
+        }
+        public void Depositar(double valorDeposito)
+        {
+             Saldo = Saldo + valorDeposito;
+
+        Console.WriteLine($"Depósito efetuado com sucesso! Seu saldo atual é de R$ {Saldo}");
+        }
+
+        public void Sacar(double valorSaque)
+        {
+            if (Saldo >= valorSaque)
+            {
+                Saldo = Saldo - valorSaque;
+                Console.WriteLine($"Saque efetuado com sucesso! Seu saldo atual é de R$ {Saldo}");
+                return;
+            }
+            Console.WriteLine("Saldo insuficiente");
+            
+        }
+
+    }
+
+}
+
---
+++ Projeto Conta Corrente/Conta Corrente/Conta Corrente/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Conta_Corrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1000, 123, "Ellie");
+
+            Console.WriteLine(conta.Saldo);
+            conta.Depositar(500);
+            conta.Sacar(200);
+        }
+    }
+}
